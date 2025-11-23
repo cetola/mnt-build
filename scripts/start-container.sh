6449e49992ad40746607b7bfc3f61dea0b7d8aa3
Stephano Cetola <stephanoc@gmail.com>
2025-11-23 10:37:44 -0800

    Update build directory for container
    Signed-off-by: Stephano Cetola <stephanoc@gmail.com>

--- scripts/start-container.sh
+++ scripts/start-container.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
 docker run -it --rm \
-    -v ~/mnt-build:/build \
-    -w /build/linux \
+    -v ~/mnt-build:/home/builder/mnt-build \
     arch-kernel-builder \
     bash
