2fa89e023551331335e1987b5428798a0b78fd8a
Stephano Cetola <stephanoc@gmail.com>
2025-11-23 20:41:55 -0800

    Cleanup upgrade script and add instructions
    Signed-off-by: Stephano Cetola <stephanoc@gmail.com>

--- scripts/upgrade-pocket.sh
+++ scripts/upgrade-pocket.sh
@@ -6,30 +6,28 @@ export MNTVER=6.17.8
 export KVER=$MNTVER-dirty
 echo "Extracting..."
 tar -xvzf kernel-$MNTVER-mnt.tar.gz
-sync && sync
 echo "Copy kernel modules: $KVER"
 sudo cp -r lib/modules/$KVER /lib/modules/
 sudo mkdir /lib/modules/$KVER/extra
 sudo cp reform2_lpc.ko /lib/modules/$KVER/extra/
 sudo cp wlan.ko /lib/modules/$KVER/extra/
-sync && sync
 echo "Copy the firmware and blacklist files."
 sudo cp -r ./usr /
 sudo cp -r ./etc /
-sync && sync
 echo "Backup old image, initramfs, and dtb"
 sudo cp /boot/Image-testing /boot/Image-old
 sudo cp /boot/initramfs-linux-testing /boot/initramfs-linux-old
 sudo cp /boot/imx8mp-mnt-pocket-reform.dtb /boot/imx8mp-mnt-pocket-reform-old.dtb
-sync && sync
 echo "Building dracut for kernel: $KVER"
 sudo dracut -f -v --kver "$KVER" --no-hostonly
-sync && sync
 echo "Install new kernel, initramfs, and dtb"
 sudo cp arch/arm64/boot/Image /boot/Image-testing
 sudo cp imx8mp-mnt-pocket-reform-$MNTVER.dtb /boot/imx8mp-mnt-pocket-reform.dtb
 sudo mv /boot/initramfs-$KVER.img /boot/initramfs-linux-testing
-sync && sync
 sudo depmod $KVER
-sync && sync
-echo "Done. Reboot and good luck."
+sync
+echo "==> IMPORTANT: Update /boot/extlinux/extlinux.conf to boot this kernel."
+echo "==> See extlinux.conf.example for reference."
+echo ""
+echo "==> Please update your bootloader configuration if needed."
+echo "==> Reboot to use the new kernel."
