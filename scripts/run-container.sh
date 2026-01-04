c358869a9da555a70c4669825fc88881e23edc06
Stephano Cetola <stephanoc@gmail.com>
2026-01-04 12:26:40 -0800

    run-container.sh: Add docker flags option
    Signed-off-by: Stephano Cetola <stephanoc@gmail.com>

--- scripts/run-container.sh
+++ scripts/run-container.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 # SPDX-License-Identifier: MIT
+set -euo pipefail
+
 MOUNT_PATH="${GITHUB_WORKSPACE:-$HOME/mnt-build}"
+DOCKER_FLAGS="${DOCKER_FLAGS:-}"
 
 if [ $# -eq 0 ]; then
-    docker run --rm -it \
+    docker run --rm -it $DOCKER_FLAGS \
         -v "$MOUNT_PATH:/home/builder/mnt-build" \
         -w /home/builder/mnt-build \
         arch-kernel-builder \
         bash
 else
-    docker run --rm \
+    docker run --rm $DOCKER_FLAGS \
         -v "$MOUNT_PATH:/home/builder/mnt-build" \
         -w /home/builder/mnt-build \
         arch-kernel-builder \
         bash -c "cd scripts && $*"
 fi
+
