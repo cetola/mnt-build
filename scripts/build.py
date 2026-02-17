62bc7b37be00dfe4552fb2c85cae0582b07384e5
Stephano Cetola <stephanoc@gmail.com>
2026-02-16 16:52:54 -0800

    mnt-build 0.4.1 - Kernel 6.18.10-1
    Signed-off-by: Stephano Cetola <stephanoc@gmail.com>

--- scripts/build.py
+++ scripts/build.py
@@ -17,7 +17,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import List, Optional, Tuple
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 
 DEFAULT_KERNEL_VERSION = '6.18.10'
 DEFAULT_PKGREL = 1
