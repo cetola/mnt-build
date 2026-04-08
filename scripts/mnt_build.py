067c3cf66994a470a8b7a02643ebaf6a9fcf351d
Stephano Cetola <stephano@cetola.net>
2026-04-08 12:10:05 -0700

    mnt-build 0.9.5 - Kernel 6.19.11.reform3
    Signed-off-by: Stephano Cetola <stephano@cetola.net>

--- scripts/mnt_build.py
+++ scripts/mnt_build.py
@@ -23,7 +23,7 @@ from config import (
 from errors import BuildError
 from logging_setup import Colors, setup_logging
 
-__version__ = "0.9.4"
+__version__ = "0.9.5"
 
 
 def run_build(version: str = DEFAULT_KERNEL_VERSION, build_dir: Optional[Path] = None,
