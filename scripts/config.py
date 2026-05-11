f122b27332843fc06a42ae6f2b15028cd13caf2a
Stephano Cetola <stephano@cetola.net>
2026-05-11 14:00:05 -0700

    bump the kernel to v7.0.6
    Signed-off-by: Stephano Cetola <stephano@cetola.net>

--- scripts/config.py
+++ scripts/config.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from pathlib import Path
 from typing import Optional
 
-DEFAULT_KERNEL_VERSION = '7.0.5'
+DEFAULT_KERNEL_VERSION = '7.0.6'
 DEFAULT_LOCALVERSION_NAME = 'reform'
 DEFAULT_LOCALVERSION_REV = 1
 DEFAULT_CROSS_COMPILE = "aarch64-linux-gnu-"
